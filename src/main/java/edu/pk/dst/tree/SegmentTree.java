2c9bfd4b5750ed8d83209e2a0425650eaefb1c3a
Prabhat Kumar <jenaprabhat@gmail.com>
2024-07-10 16:48:12 +0530

    Made it generic

--- src/main/java/edu/pk/dst/tree/SegmentTree.java
+++ src/main/java/edu/pk/dst/tree/SegmentTree.java
@@ -1,17 +1,23 @@
-public class SegmentTree {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiFunction;
 
-  private int[] tree;
-  private int n;
+public class SegmentTree<T> {
+
+  private final List<T> tree;
+  private final int n;
+  private final BiFunction<T, T, T> operation;
 
   /**
    * Constructs a Segment Tree from the given array.
    *
    * @param arr the input array
    */
-  public SegmentTree(int[] arr) {
+  public SegmentTree(T[] arr, BiFunction<T, T, T> function) {
     this.n = arr.length;
-    this.tree = new int[2 * n];
-    buildTree(arr);
+    this.tree = new ArrayList<>(2 * n);
+    this.operation = function;
+    buildTree(arr, this.operation);
   }
 
   /**
@@ -19,12 +25,18 @@ public class SegmentTree {
    *
    * @param arr the input array
    */
-  private void buildTree(int[] arr) {
+  private void buildTree(T[] arr, BiFunction<T, T, T> func) {
     // Insert leaf nodes in the tree
-    System.arraycopy(arr, 0, tree, n, n);
+    //System.arraycopy(arr, 0, tree, n, n);
+    for (int i = 0; i < n; i++) {
+      tree.add(i, null);
+    }
+    for (int i = n; i < 2 * n; i++) {
+      tree.add(i, arr[i - n]);
+    }
     // Build the tree by calculating parents. Update parent starting from children
     for (int i = n - 1; i > 0; i--) {
-      tree[i] = tree[i * 2] + tree[i * 2 + 1];
+      tree.set(i, func.apply(tree.get(i * 2), tree.get(i * 2 + 1)));
     }
   }
 
@@ -34,14 +46,14 @@ public class SegmentTree {
    * @param index the index to update
    * @param value the new value
    */
-  public void update(int index, int value) {
+  public void update(int index, T value) {
     // Update the value at the leaf node
     index += n;
-    tree[index] = value;
+    tree.set(index, value);
     // Update corresponding parent nodes
     while (index > 1) {
       index /= 2;
-      tree[index] = tree[index * 2] + tree[index * 2 + 1];
+      tree.set(index, operation.apply(tree.get(index * 2), tree.get(index * 2 + 1)));
     }
   }
 
@@ -52,8 +64,8 @@ public class SegmentTree {
    * @param right the right index (exclusive)
    * @return the sum in the range [left, right)
    */
-  public int query(int left, int right) {
-    int sum = 0;
+  public T query(int left, int right) {
+    T sum = null;
     // Move left, right to leaf node side which are the actual input elements
     left += n;
     right += n;
@@ -61,13 +73,13 @@ public class SegmentTree {
       /* if left index is a right node of its parent, then consider that element
       and jump to parent of its next element in range. */
       if (left % 2 == 1) {
-        sum += tree[left];
+        sum = operation.apply(sum, tree.get(left));
         left++;
       }
       /* if right index is a left node of its parent, then consider that element
       and jump to parent of its previous element in range. */
       if (right % 2 == 0) {
-        sum += tree[right];
+        sum = operation.apply(sum, tree.get(right));
         right--;
       }
       left /= 2;
@@ -77,12 +89,18 @@ public class SegmentTree {
   }
 
   public static void main(String[] args) {
-    int[] arr = {1, 3, 5, 7, 9, 11};
-    SegmentTree segmentTree = new SegmentTree(arr);
-    System.out.println(segmentTree.query(0, 4)); // Output: 25
-    System.out.println(segmentTree.query(1, 3)); // Output: 15
-    System.out.println(segmentTree.query(3, 5)); // Output: 15
-    segmentTree.update(1, 10);
-    System.out.println(segmentTree.query(1, 5)); // Output: 42
+    Integer[] arr = {5, 11, 1, 7, 9, 3};
+    SegmentTree<Integer> maxSegmentTree = new SegmentTree<>(arr, (a, b) -> {
+      if (a == null) {
+        return b;
+      }
+      return Math.max(a, b);
+    });
+    System.out.println(maxSegmentTree.query(0, 4)); // Output: 25
+    System.out.println(maxSegmentTree.query(1, 3)); // Output: 15
+    System.out.println(maxSegmentTree.query(3, 5)); // Output: 15
+    System.out.println(maxSegmentTree.query(1, 5)); // Output: 42
+    maxSegmentTree.update(1, 8);
+    System.out.println(maxSegmentTree.query(1, 5)); // Output: 42
   }
 }
