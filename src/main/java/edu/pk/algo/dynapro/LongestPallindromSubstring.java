65809a4aa230f0f76acb5d57e84510b0644c9057
Prabhat Kumar <jenaprabhat@gmail.com>
2024-07-11 10:41:41 +0530

    Update LongestPallindromSubstring.java

--- src/main/java/edu/pk/algo/dynapro/LongestPallindromSubstring.java
+++ src/main/java/edu/pk/algo/dynapro/LongestPallindromSubstring.java
@@ -17,57 +17,68 @@ import edu.pk.Util;
  */
 public class LongestPallindromSubstring {
 	public static void main(String[] args) {
-		String str = "abbdmalayalambb";
-		char[] chars = str.toCharArray();
-		int len = chars.length;
-		Boolean s[][] = new Boolean[len][len];
-        // fills left main diagonal and next left small diagonal (\) i.e for each substring of length 2
-		for (int i = 0; i < len; i++) {
-			s[i][i] = true; // base case, to fill left diagonal
-			if (i < len - 1) // for up to previous of the last
-				if (chars[i] == chars[i + 1])
-					s[i][i + 1] = true;
-				else
-					s[i][i + 1] = false;
+		String s = "XAABADAABAAY";
+		System.out.println(longestPalindromLength(s.toCharArray(), 0, s.length() - 1));
+		final Pair<String, Integer> pair = longestPalindromByDP(s);
+		System.out.println("Max pallindrom length = " + pair.getValue());
+		System.out.println("Longest pallindrom string = " + pair.getKey());
+	}
+	
+	/* Dynamic programming approach */
+	private static Pair<String, Integer> longestPalindromByDP(String str) {
+		int pallindromLength = 1;
+		int pallindromStartIndex = 0;
+		final char[] array = str.toCharArray();
+		boolean[][] dp = new boolean[array.length][array.length];
+		// fill diagonal
+		for (int i = 0; i < array.length; i++) {
+			dp[i][i] = true;
 		}
-
-        // filling the remaining diagonals (diagonally)
-		int pallindromStartIndex = -1;
-		int pallindromEndIndex = 0;
-		for (int l = 2; l < len; l++) {
-			for (int r = 0; r < len - l; r++) {
-				int c = r + l;
-				s[r][c] = (s[r + 1][c - 1] && chars[r] == chars[c]);
-				if (s[r][c]) {
-					pallindromStartIndex = r;
-					pallindromEndIndex = c;
+		// fill (0,1) (1,2) (2,3) adjacent entries for substring length = 2
+		for (int i = 1; i < array.length - 1; i++) {
+			if (array[i] == array[i - 1]) {
+				dp[i - 1][i] = true;
+				pallindromLength = 2;
+			}
+		}
+		// fill for length 3 to string length
+		for (int len = 2; len < array.length; len++) {
+			for (int i = 0; i < array.length - len; i++) {
+				if (array[i] == array[i + len] && dp[i + 1][i + len - 1]) {
+					dp[i][i + len] = true;
+					pallindromLength = len + 1;
+					pallindromStartIndex = i;
 				}
 			}
 		}
-		print(chars, s, pallindromStartIndex);
-		System.out.println("\nLongest Pallindrom Substring : " + str.substring(pallindromStartIndex, pallindromEndIndex + 1));
+		return new Pair<>(str.substring(pallindromStartIndex, pallindromStartIndex + pallindromLength), pallindromLength);
 	}
-
-	private static void print(char[] chars, Boolean a[][], int start) {
-		System.out.println("Dynamic Values : \n");
-		for (char c : chars) {
-			System.out.printf("%3s", c);
+	
+	/* Recursion method */
+	public static int longestPalindromLength(char[] str, int i, int j) {
+		if (i == j) {
+			return 1;
 		}
-		System.out.println();
-		for (int i = 0; i < chars.length; i++) {
-			System.out.printf("%s", "___");
+		if (str[i] == str[j] && i + 1 == j) {
+			return 2;
 		}
-		System.out.println();
-		for (int i = 0; i < a.length; i++) {
-			System.out.print(chars[i] + "|");
-			for (int j = 0; j < a[i].length; j++) {
-				System.out.print((a[i][j] == null ? " " : (a[i][j] == true ? 1 : 0)) + "  ");
-			}
-			if (i == start) {
-				System.out.print(" <--- LPS ");
-			}
-			System.out.println();
+		int l = 0;
+		if (str[i] == str[j] && (l = isPallindrom(str, i + 1, j - 1)) > 0) {
+			return l + 2;
 		}
-
+		return Math.max(longestPalindromLength(str, i, j - 1), longestPalindromLength(str, i + 1, j));
 	}
+	
+	private static int isPallindrom(char[] str, int i, int j) {
+		int l = j - i + 1;
+		while (i <= j) {
+			if (str[i] != str[j]) {
+				return 0;
+			}
+			i++;
+			j--;
+		}
+		return l;
+	}
+	
 }
